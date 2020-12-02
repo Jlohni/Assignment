e73c04a324f9ad589b88208f61b985b5d1daf319
jagdish <jlohni@gmail.com>
2020-12-02 21:41:48 +0530

    Added App.js

--- Assignment-2 React/my-app/src/App.js
+++ Assignment-2 React/my-app/src/App.js
@@ -1,35 +1,61 @@
 import React, { Component } from 'react'
 import './App.css';
+import Note from './Components/Node';
+import Node from './Components/Node'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       noteText: '',
+      notes: [],
     }
 
   }
   updateNoteText(noteText) {
     this.setState({ noteText: noteText.target.value})
   }
+
+  addNote() {
+    if ( this.state.noteText === '') {return}
+
+    let notesArr = this.state.notes;
+    notesArr.push( this.state.noteText );
+    this.setState({ noteText: '' });
+    this.textInput.focus();
+  }
   handleKeyPress = (Event) => {
     if (Event.key === 'Enter') {
+      let notesArr = this.state.notes;
+      notesArr.push( this.state.noteText );
+      this.setState({ noteText: '' });
 
     }
   }
-  
+  deleteNote(index) {
+    let notesArr = this.state.notes;
+    notesArr.splice(index, 1);
+    this.setState({ notes: notesArr})
+  }
 
 
   render() {
+    let notes = this.state.notes.map((val, key) => {
+      return <Note key={key} text={val}
+         deleteMethod={ () => this.deleteNote(key)} />
+    })
+
     return (
       <div className="container">
-        <div className="header"> React Todo Application </div>
-        <div className="btn">+</div>
-        <input type="text"
+        <div className="header"> <h1>To-Do List</h1> <p> Creat your Daily Work List</p> </div>
+
+        {notes}
+        <div className="btn" onClick={this.addNote.bind(this)}>+</div>
+        <input type="text" placeholder="Creat Your schedule"
         ref={((input) => {this.textInput = input})} 
         className="textInput"
         value={this.state.noteText}
-        onChangeText={noteText => this.updateNoteText(noteText)}
+        onChange={noteText => this.updateNoteText(noteText)}
         onKeyPress={this.handleKeyPress.bind(this)}
         
         />
