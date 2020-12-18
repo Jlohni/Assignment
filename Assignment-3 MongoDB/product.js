59d768612eed066b00f91630ef1de54c5c2b1976
jagdish <jlohni@gmail.com>
2020-12-18 22:59:56 +0530

    Added product schema in product.js

---
+++ Assignment-3 MongoDB/product.js
@@ -0,0 +1,22 @@
+db.products.insert({
+  title: "GW Athlete T-shirt",
+  description: "Best Quality T-shirt Attractive locking",
+
+  manufacture_details: {
+    Brand : "Gorillawear",
+   
+  },
+
+  shipping_details: {
+    weight: 300,
+    width: 10,
+    height: 10,
+    depth: 1
+  },
+
+  quantity: 99,
+
+  pricing: {
+    price: 5100
+  }
+});
